162097efc0d3cff4f8d1b9666638acdc9367747e
valiwisdev <v.caro@uniandes.edu.co>
2025-12-11 21:58:12 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,32 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "EduEX",
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: "EDUEX",
+            link: "/eduex/",
+            items: [
+              {
+                label: "EDUEX",
+                autogenerate: { directory: "eduex" },
+              },
+            ],
+          },
+          {
+            label: "CSA",
+            link: "/csa/",
+            items: [{ label: "CSA", autogenerate: { directory: "csa" } }],
+          },
+        ]),
+      ],
+    }),
+  ],
 });
